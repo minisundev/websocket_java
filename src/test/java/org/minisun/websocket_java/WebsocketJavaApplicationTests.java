dafa232c7723dc2fdf9c1d8eb34fa5d19ee3e2d3
minisun <minisundev@gmail.com>
2024-08-21 23:12:49 +0900

    feat: init commit

---
+++ src/test/java/org/minisun/websocket_java/WebsocketJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.minisun.websocket_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
